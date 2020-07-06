2f4cc5f15ffa8ed2d0be686ca09627abe9b7dfd8
EricHong <f102041332@gmail.com>
2020-07-06 23:48:41 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -17,11 +17,16 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/member',
+    name: 'Member',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Member.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,28 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloCool msg="Welcome to Your Cool.js"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import HelloCool from '@/components/HelloCool.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloCool,
+    HelloWorld,
+  },
+  mounted(){
+    fetch("./api/bkSetFind.php")
+    .then(res => res.json())
+    .then(rs => {
+        const bandowns = rs;
+        console.log(bandowns);
+    })
   }
 }
 </script>
