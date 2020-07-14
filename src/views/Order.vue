3d5fd0617bb0000e24c93d857eda21ef17649bea
jimmy5217123 <jimmy5217123@gmail.com>
2020-07-15 00:36:21 +0800

    wow

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -6,8 +6,8 @@
      <div id="buy_app3">
                 <div class="buy_setdo_containter scroll">
       
-                    <div class="buy_card3" v-for="item in orderSetdo" :key="item">
-                        <div v-if="item.setStatus==1 && item.setClass ==0">
+                    <div  v-for="item in orderSetdo" :key="item">
+                        <div v-if="item.setStatus==1 && item.setClass ==0" class="buy_card3">
                           <img :id="'setdoimg'+item.setId"  :data-id="item.setId" v-bind:src="item.setImage" atl="" width="200" height="200">
                           <div class="buy_name">{{item.setName}}&nbsp;&nbsp;<span>$</span>{{item.setPrice}}</div>
                           <div><button class="orderbutton" :id ="'setdocountminus'+item.setId" :data-num="item.setId">-</button><span class="ordercount" :id="'setdocount'+item.setId">0</span><button class="orderbutton" :id ="'setdocountplus'+item.setId" :data-num="item.setId">+</button>
@@ -17,18 +17,11 @@
 
 
 
-                        <div class="buy_single_lightbox box3">
-                            <div class="orderrelitive">
-                                <h3 id="setdolightTitle"></h3>  
-                                <img src="" alt="" width="200" height="200" id="setdolightImg">
-                                <p id="setdolightText"></p>
-                                <img src="../images/order/close.svg" class="closelightbox3" width="15" height="15">
-                            </div>
-                        </div>
+                    
                     </div>
 
-                    <div class="buy_card3" v-for="item in orderSetdo" :key="item">
-                        <div v-if="item.setStatus==1 && item.setClass ==1">
+                    <div  v-for="item in orderSetdo" :key="item">
+                        <div v-if="item.setStatus==1 && item.setClass ==1" class="buy_card3">
                           <img :id="'setdoimg'+item.setId"  :data-id="item.setId" v-bind:src="item.setImage" atl="" width="200" height="200">
                           <div class="buy_name">{{item.setName}}&nbsp;&nbsp;<span>$</span>{{item.setPrice}}</div>
                           <div><button class="orderbutton" :id ="'setdocountminus'+item.setId" :data-num="item.setId">-</button><span class="ordercount" :id="'setdocount'+item.setId">0</span><button class="orderbutton" :id ="'setdocountplus'+item.setId" :data-num="item.setId">+</button>
@@ -38,20 +31,12 @@
 
 
 
-                        <div class="buy_single_lightbox box3">
-                            <div class="orderrelitive">
-                                <h3 id="setdolightTitle"></h3>  
-                                <img src="" alt="" width="200" height="200" id="setdolightImg">
-                                <p id="setdolightText"></p>
-                                <img src="../images/order/close.svg" class="closelightbox3" width="15" height="15">
-                            </div>
-                        </div>
+            
                     </div>
            
                    
 
 
-                   </div>
                  
                 </div>
       
@@ -61,18 +46,72 @@
 </template>
 
 <script>
+let orderSetdo=[];
+
+
+
+fetch("./php/memajax3.php")
+    .then(res => res.json())
+    .then((res)=>{
+      console.log(res)
+      // this.orderSetdo = res[0];
+      orderSetdo = res[0];
+      console.log(orderSetdo);
+      // this.memOtherProduct = res[1];
+      // console.log(this.memOtherProduct);
+    })
+
+export default {
+  data(){
+    return {
+      orderSetdo:[],
+      memOtherProduct:[],
+    }
+  },
+  mounted(){
+    // $.ajax({
+    //       type: "GET",
+    //       url: "./php/memajax3.php",
+    //       success: function (response) {
+    //           this.memSetProduct = JSON.parse(response)[0];
+    //           this.memOtherProduct = JSON.parse(response)[1];
+    //           console.log(this.memOtherProduct);
+    //       }
+    //   });
+    fetch("./php/memajax3.php")
+    .then(res => res.json())
+    .then((res)=>{
+      console.log(res)
+      this.orderSetdo = res[0];
+      orderSetdo = res[0];
+      console.log(orderSetdo);
+      this.memOtherProduct = res[1];
+      console.log(this.memOtherProduct);
+    })
+  // this.$nextTick(function () {
+  //     axios.get('./php/memajax3.php').then(function(res){
+  //     Vue.$data.memSetProduct = res.data[0];
+  //   })
+  //   })
+  }
+}
+
+ setTimeout(function() {
+    
+ 
 
     
-   
+     console.log(orderSetdo);
         let setdocount=[];
         var setdoMany = 0;
         var setdoId = 0;
         var setdoName = 0;
         var setdoPrice = 0;
         var setdoImg = 0;
-        let orderSetdo = [];
+  
         var setdoNum = 0;
         let setdoMenu=[];
+  
       
 for(var j=0; j < orderSetdo.length;j++){    //套餐的+- 購物車   click事件
     if(orderSetdo[j].setStatus==1){
@@ -218,42 +257,89 @@ function setdoCart(){      //套餐的加入購物車
 // import Vue from 'vue';
 // import $ from 'jquery';
 // import axios from 'axios';
-export default {
-  data(){
-    return {
-      orderSetdo:[],
-      memOtherProduct:[],
-    }
-  },
-  mounted(){
-    // $.ajax({
-    //       type: "GET",
-    //       url: "./php/memajax3.php",
-    //       success: function (response) {
-    //           this.memSetProduct = JSON.parse(response)[0];
-    //           this.memOtherProduct = JSON.parse(response)[1];
-    //           console.log(this.memOtherProduct);
-    //       }
-    //   });
-    fetch("./php/memajax3.php")
-    .then(res => res.json())
-    .then((res)=>{
-      console.log(res)
-      this.orderSetdo = res[0];
-      orderSetdo = res[0];
-      this.memOtherProduct = res[1];
-      console.log(this.memOtherProduct);
-    })
-  // this.$nextTick(function () {
-  //     axios.get('./php/memajax3.php').then(function(res){
-  //     Vue.$data.memSetProduct = res.data[0];
-  //   })
-  //   })
-  }
-}
+ }, 500);
 </script>
 
 <style lang="scss">
+
+    #buy_app3 {
+  height: 650px;
+  font-family: 'Noto Sans TC','Noto Sans', '微軟正黑體', "Times New Roman", sans-serif;
+  // overflow-y: auto;
+
+
+.buy_setdo_containter {
+  flex-wrap: wrap;
+  width: 1050px;
+  margin: 0 auto;
+  height: 350px;
+  display: flex;
+  // justify-content: space-between;
+}
+
+.buy_card3 {
+  width: 250px;
+  height: 308px;
+  margin: 10px 50px;
+  text-align: center;
+  border-radius: 10px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+  position: relative;
+
+  // &:hover {
+    // box-shadow: 2px 2px 10px $orange;
+  // }
+ 
+  .cart {
+    // @include btn(auto, auto);
+    transition-duration: 0.2s;
+    transition-property: all;
+    transform-origin: 50% 50%;
+    color: white;
+    background-color: #EA6227;
+    font-family: "Noto Sans TC";
+    font-size: 14px;
+    width:180px;
+    font-weight: 700;
+    letter-spacing: 2px;
+    padding: 5px 10px;
+    border-radius: 5px;
+    
+  }
+
+  img {
+    margin-top: 5px;
+    cursor: pointer;
+  }
+
+  p {
+    margin: 0;
+    // color: $grey;
+  }
+
+  .orderbutton {
+    cursor: pointer;
+    border-radius: 5px;
+    margin: 5px 5px;
+    width: 23px;
+    height: 23px;
+    font-size: 20px;
+  
+    background-color: #37AB64;
+
+    border: none;
+    color: #fff;
+
+    &:hover {
+      background-color: #40dd7c;
+    }
+  }
+  .ordercount{
+    font-size: 18px;
+  }
+}
+
+}
 
 </style>
 
